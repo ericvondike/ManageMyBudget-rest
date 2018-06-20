c437feaaf864d2a11888e643b119bbf72cbd07f2
edaklan <ericvondike@gmail.com>
2018-06-20 23:29:07 +0200

    US-01: The problem of the delet button solved. No I should prevent the page from scrolling up when the submit buttons for adding and removing the items and categories are clicked.

--- src/main/java/com/daklan/budget/control/rest/controllers/FormBasketControler.java
+++ src/main/java/com/daklan/budget/control/rest/controllers/FormBasketControler.java
@@ -8,33 +8,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Controller
 public class FormBasketControler {
     private final Logger LOGGER = LoggerFactory.getLogger(FormBasketControler.class);
-
-    ShoppingListIn shoppingListIn;
-
-    public FormBasketControler(ShoppingListIn shoppingListIn) {
-        super();
-        this.shoppingListIn = shoppingListIn;
-    }
-
-    public ShoppingListIn getShoppingListIn() {
-        return shoppingListIn;
-    }
-
-    public void setShoppingListIn(ShoppingListIn shoppingListIn) {
-        this.shoppingListIn = shoppingListIn;
-    }
-
 
     @ModelAttribute("allShoppingCenters")
     public List<ShoppingCenter> populateShoppingCenters() {
@@ -48,33 +32,29 @@ public class FormBasketControler {
 
     @RequestMapping(value = "/basketform")
     public String showBasketForm(Model model) {
-
         ShoppingListIn shoppingListIn = new ShoppingListIn();
-        this.shoppingListIn = shoppingListIn;
         model.addAttribute("basketForm", shoppingListIn);
 
         return "basketform";
     }
 
     @RequestMapping(value="/basketform", params={"addCategory"})
-    public String addRow(final ShoppingListIn shoppingListIn, final BindingResult result, final Model model) {
+    public String addCategory(final ShoppingListIn shoppingListIn, final BindingResult result, final Model model) {
         CategoryIn categoryIn = new CategoryIn();
         shoppingListIn.getCategoryInList().add(categoryIn);
         model.addAttribute("category", categoryIn);
         model.addAttribute("basketForm", shoppingListIn);
-        this.shoppingListIn = shoppingListIn;
 
         return  "basketform";
     }
 
     @RequestMapping(value = "/basketform", params={"removeCategory"})
-    public String removeRow(final ShoppingListIn shoppingListIn, final BindingResult bindingResult,
+    public String removeCategory(final ShoppingListIn shoppingListIn, final BindingResult bindingResult,
                             final HttpServletRequest req, final Model model){
         final Integer rowId = Integer.valueOf(req.getParameter("removeCategory"));
         List<CategoryIn> category = shoppingListIn.getCategoryInList();
         category.remove(rowId.intValue());
         model.addAttribute("basketForm", shoppingListIn);
-        this.shoppingListIn = shoppingListIn;
 
         return "basketform";
     }
@@ -87,25 +67,34 @@ public class FormBasketControler {
         CategoryIn categoryIn = shoppingListIn.getCategoryInList().get(rowId.intValue());
         categoryIn.getItemsList().add(new ItemIn());
         model.addAttribute("basketForm", shoppingListIn);
-        this.shoppingListIn = shoppingListIn;
 
         return "basketform";
     }
 
 
-    @RequestMapping(path = "/basketform/{categoryIndex}/{itemIndex}")
-    public String removeItem(final ShoppingListIn shoppingListInp,
-                             @PathVariable("categoryIndex") String categoryIndex,
-                             @PathVariable("itemIndex") String itemIndex,
-                             final BindingResult bindingResult,
-                             final Model model) throws IOException {
-        final Integer catInd = Integer.valueOf(categoryIndex);
-        final Integer itemInd = Integer.valueOf(itemIndex);
-        ShoppingListIn shoppingListIn = this.shoppingListIn;
-        CategoryIn categoryIn = shoppingListIn.getCategoryInList().get(catInd.intValue());
-        categoryIn.getItemsList().remove(itemInd.intValue());
-        this.shoppingListIn = shoppingListIn;
-        model.addAttribute("basketForm",shoppingListIn);
+
+
+    @RequestMapping(value = "/basketform", params={"removeItem"})
+    public String removeItem(final ShoppingListIn shoppingListIn,
+                          final BindingResult bindingResult,
+                          final HttpServletRequest req, final Model model){
+        final String catItem = req.getParameter("removeItem");
+        String strCatIndex = null;
+        String strItemIndex = null;
+        Pattern pattern = Pattern.compile("-");
+        Matcher matcher = pattern.matcher(catItem);
+        if(matcher.find()) {
+            strCatIndex = catItem.substring(0, matcher.start());
+            strItemIndex = catItem.substring(matcher.end());
+        }
+        if (strCatIndex == null || strItemIndex == null) {
+            throw new IllegalArgumentException ("The value of catIndex cannot be null");
+        }
+        final int catIndex = Integer.parseInt(strCatIndex);
+        final int itemIndex = Integer.parseInt(strItemIndex);
+        CategoryIn categoryIn = shoppingListIn.getCategoryInList().get(catIndex);
+        categoryIn.getItemsList().remove(itemIndex);
+        model.addAttribute("basketForm", shoppingListIn);
 
         return "basketform";
     }
